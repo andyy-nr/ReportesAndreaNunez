5f8fcd09d9f2696c9857f2dfc6a345900f3fe465
Ingenieria Sistemas11 <isi11@lab.uca.edu.ni>
2023-05-24 16:01:04 -0600

    Primera parte de la guia

---
+++ Reportes/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
+
+Public Class Form1
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+            frmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            frmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            frmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            frmVistaPrevia.ReportViewer1.Refresh()
+            frmVistaPrevia.ReportViewer1.Show()
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+        End Try
+
+    End Sub
+
+    Private Sub btnImprimirCargos_Click(sender As Object, e As EventArgs) Handles btnImprimirCargos.Click
+        Try
+            Dim tsql As String = "Select * from jobs"
+            Dim conex As New SqlConnection(My.Settings.cstrBDRH)
+            Dim da As New SqlDataAdapter(tsql, conex)
+            Dim t As New DataTable
+            da.Fill(t)
+            verReporte(t, "dsRegistros", "diseñosRpt\rptCargo.rdlc")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al cargar reporte")
+        End Try
+    End Sub
+End Class
---
+++ Reportes/Formularios/frmVistaPrevia.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.Reporting.WinForms
+
+Public Class frmVistaPrevia
+    Private Sub frmVistaPrevia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+
+End Class
